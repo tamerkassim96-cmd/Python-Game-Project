0973ba7d63710b9c5a9fa0d682153e0a5b1c09ef
tamerkassim96-cmd <tamerkassim96@gmail.com>
2025-10-14 04:42:35 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,8 +1,14 @@
-import sys
-import time
+import sys, time
 
-def slow_text(text, min_delay =0.05):
+def slow_text(text, min_delay = 0.001):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(min_delay)
+
+def slow_print(text, delay=1, new_line=True):
+    slow_text(text)
+    time.sleep(delay)
+    print()
+    if new_line:
+        print()
