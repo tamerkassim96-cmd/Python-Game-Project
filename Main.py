c0b329539b0500f50ad4e40724312fa83fa6823a
tamerkassim96-cmd <tamerkassim96@gmail.com>
2025-10-18 17:42:57 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -191,7 +191,7 @@ def input_name():
         name = input('Please enter your name: ')
         if name == "" and name.isalnum(): # This makes sure if the user enters special characters or any empty spaces then it would say the name is invalid and to try again
             return name
-            slow_print('Invalid Choice, no special characters or spaces. Try again.')
+            slow_print('Invalid input, no special characters or spaces. Try again.')
             continue
         else:
             slow_print(f'\n{name} Eh? a fine name for a {Fore.LIGHTYELLOW_EX}Radiant{Fore.LIGHTBLUE_EX} I mean warrior')
